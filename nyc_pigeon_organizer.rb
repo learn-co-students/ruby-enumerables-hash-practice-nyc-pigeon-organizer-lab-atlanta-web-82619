8c9ffe3d1420498be40c94aaa2757fd10fbde8e6
Nicholas Lunn <nrlunn325@gmail.com>
2019-08-17 14:56:17 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,24 @@
+require "pry"
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  dem_pidgeys = {}
+  data.each do |pidgey_property, hash|
+    hash.each do |pidgey_attribute, array|
+      array.each do |pidgey|
+        
+        if !dem_pidgeys.has_key?(pidgey)
+          dem_pidgeys[pidgey] = {}
+        end
+        
+        if !dem_pidgeys[pidgey].has_key?(pidgey_property)
+          dem_pidgeys[pidgey][pidgey_property] = []
+        end
+        
+        if !dem_pidgeys[pidgey][pidgey_property].include?(pidgey_attribute)
+          dem_pidgeys[pidgey][pidgey_property] << pidgey_attribute.to_s
+        end
+      end
+    end
+  end
+  dem_pidgeys
+  #binding.pry
 end
