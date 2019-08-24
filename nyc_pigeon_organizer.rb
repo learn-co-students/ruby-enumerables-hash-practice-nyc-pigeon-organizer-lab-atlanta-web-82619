9dec89114f2ecc7479eb9ae267f46eff83850cb9
Mark Del Mar <mark@1000martians.com>
2019-08-24 20:30:11 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,32 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |color_gender_lives, value|
+    value.each do |stats, all_names|
+      all_names.each do |name|
+        if pigeon_list[name] == nil
+          pigeon_list[name] = {}
+        end
+        if pigeon_list[name][color_gender_lives] == nil
+          pigeon_list[name][color_gender_lives] = []
+        end
+        pigeon_list[name][color_gender_lives].push(stats.to_s)
+      end
+    end 
+  end
+  pigeon_list
 end
+
+
+
+# include vs has_value?
+# moving data into nil value
+# .to_s
+
+
+
+
+# pigeon_list[:name] || = {} checks existence and sets it
+# if !pigeon_list[:name] // checks existence 
+# Or Equals operator
