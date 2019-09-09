e5057c0bad0705c6315766f4a384d282fa5b300e
vibraniumforge <gregm9999@gmail.com>
2019-09-09 15:08:54 -0700

    removed a puts statement

--- spec/nyc_pigeon_organizer_spec.rb
+++ spec/nyc_pigeon_organizer_spec.rb
@@ -28,7 +28,6 @@ describe "NYC Pigeon Organizer" do
 
     context "Overview: Data Type and Attributes of Return Value" do
       it "returns a hash" do
-        puts @result
         expect(@result.class).to eq(Hash)
       end
       it "returns a hash with seven keys" do
